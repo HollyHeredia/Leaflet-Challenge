9c03f1601ec8255ba333edc6818510da2ce306e5
Holly Heredia <hollyheredia@gmail.com>
2021-08-03 16:27:38 -0700

    started layergroups.

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,3 +1,7 @@
 // Define earthquakes and tectonic plates GeoJSON url variables
 var earthquakesURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 var tectonicplatesURL = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json"
+
+// Create two layerGroups
+var earthquakes = L.layerGroup();
+var tectonicplates = L.layerGroup();
